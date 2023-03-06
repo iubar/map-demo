8888158443dee5a43a0d01ef30d105e4d1a1aeef
Andrea Borgogelli Avveduti <borgogelli@iubar.it>
2023-03-06 09:33:48 +0100

    .

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -3,9 +3,9 @@ import renderer, { act } from 'react-test-renderer';
 import App from '../App';
 
 describe('<App />', () => {
-	it('has 1 child', () => {
+	it('has 2 child', () => {
 		const tree = renderer.create(<App />).toJSON();
-		expect(tree.children.length).toBe(1);
+		expect(tree.children.length).toBe(2);
 	});
 	it('matches snapshot and renders correctly', () => {
 		let tree = renderer.create(<App />);
