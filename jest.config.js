f9575a4259dca4fbf6e439abc0bbbcad3dc34bf2
Andrea Borgogelli Avveduti <borgogelli@iubar.it>
2023-03-06 09:06:40 +0100

    .

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,6 @@ const config = {
     transformIgnorePatterns: [
         "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
       ]
-    
 };
 
 module.exports = config;
