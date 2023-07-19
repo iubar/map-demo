05b7872f5c49893104251d0fbfa3cf68f91b274d
Andrea Borgogelli Avveduti <borgogelli@iubar.it>
2023-07-19 11:21:01 +0200

    .

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,6 @@
 module.exports =  {
     presets: ['babel-preset-expo'],
     plugins : [
-          // "@babel/plugin-proposal-export-namespace-from",
           // "react-native-reanimated/plugin"
         ]
 };
